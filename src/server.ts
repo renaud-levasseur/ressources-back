22875983c3f7d5995a1e3f01402401bf7cf1f49f
WRenoulleau <wil.renou@gmail.com>
2024-03-06 09:17:05 +0100

    Prerequis server.ts et sequelize.config.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import db from "../sequelize.config";
 
 const app = express();
 const port = 3000;
@@ -10,3 +11,7 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+db.authenticate()
+.then(() => console.log('Connection has been established successfully.'))
+.catch((error) => console.error('Unable to connect to the database:', error));
