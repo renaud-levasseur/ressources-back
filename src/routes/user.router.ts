c87660042c08e02ff147e9c7c90d85f8132979c9
WRenoulleau <wil.renou@gmail.com>
2024-05-11 00:22:53 +0200

    Profile

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller";
+import { createUser,updateUser,getUserById } from "../controllers/user.controller";
 
 const router = express.Router();
 
 router.post('/createSpecificUser', createUser);
+router.patch('/updateUser/:id', updateUser);
+router.get('/user/:id', getUserById);
 
 export default router; 
