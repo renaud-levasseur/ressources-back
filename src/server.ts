f81fcea20a82de9e3544e23a04c2759aa252760a
WRenoulleau <wil.renou@gmail.com>
2024-05-10 21:16:49 +0200

    gitignore et middleware

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ app.use(session({
 // Middleware
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
